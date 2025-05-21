ae00068326d14edd3d36494c44ae81df3e1fbb25
Meltallian <jeremie.bidaux@gmail.com>
2025-05-21 15:47:48 +0200

    added parsing

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Run your simulator specifically (shortcut command)
 simulator: compile
-	@docker exec -it java8-compiler java com.avaj.simulator.Simulator scenario.txt
+	@sudo docker exec -it java8-compiler java com.avaj.simulator.Simulator scenario.txt
 
 .PHONY: all clean compile run start stop view-output run-app
 
@@ -9,16 +9,16 @@ all: compile
 
 # Start the Docker container
 start:
-	docker-compose up -d
+	sudo docker compose up -d
 
 # Stop the Docker container
 stop:
-	docker-compose down
+	sudo docker compose down
 
 # Clean compiled files
 clean:
-	docker exec -it java8-compiler sh -c "find . -name \"*.class\" -delete"
+	sudo docker exec -it java8-compiler sh -c "find . -name \"*.class\" -delete"
 
 # Compile all Java files
 compile: start
-	docker exec -it java8-compiler sh -c "find * -name \"*.java\" > sources.txt && javac @sources.txt"
+	sudo docker exec -it java8-compiler sh -c "find * -name \"*.java\" > sources.txt && javac @sources.txt"
--- com/avaj/simulator/Utilities.java
+++ com/avaj/simulator/Utilities.java
@@ -1,6 +1,8 @@
 package com.avaj.simulator;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utilities {
 
@@ -17,15 +19,39 @@ public class Utilities {
 		}
 	}
 
+	public static void	pl(String str) {
+		if (!str.isEmpty()) {
+			System.out.println(str);
+		}
+		else {
+			System.out.println("Error pl call with empty string");
+		}
+	}
+
 	public static boolean	scenarioParse(List<String> lines){
 		if (lines.isEmpty()) {
-			System.out.println("Parsing error: empty scenario file.");
+			pl("Parsing error: empty scenario file.");
 			return false;
 		}
 		if (!isInt(lines.get(0))) {
-			System.out.println("Parsing error: first line must be an simple Integer. ");
+			pl("Parsing error: first line must be an simple Integer. ");
 			return false;
-		}	
+		}
+		Pattern pattern = Pattern.compile(
+			"^(Baloon|JetPlane|Helicopter)\\s" +  // Vehicle type
+			"\\w+\\s" +                           // Word characters
+			"(360|3[0-5][0-9]|[1-2][0-9][0-9]|[1-9][0-9]|[0-9])\\s" +  // 0-360
+			"(180|1[0-7][0-9]|[1-9][0-9]|[0-9])\\s" +                 // 0-180
+			"(100|[1-9][0-9]|[0-9])$",                               // 0-100
+			Pattern.COMMENTS);
+		for (int i = 1; i < lines.size(); i++) {
+			String line = lines.get(i);
+			Matcher matcher = pattern.matcher(line);
+			if (!matcher.find()) {
+				pl("Error: scenario.txt not valid");
+				return false;
+			}
+		}
 		return true;
 	}
 }
